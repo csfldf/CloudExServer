9c40a1d39e185a13afee22f3a61e7e2600e48e6f
sk <234815790@qq.com>
2016-01-29 16:02:43 +0800

    a

--- wsgi_instance/controllers.py
+++ wsgi_instance/controllers.py
@@ -21,20 +21,7 @@ class Controller(object):
 
 
     def testAction(self, req):
-        check = 1
-
-        try:
-            token = req.headers['X-Auth-Token']
-            if token != 'sk':
-                check = 0
-
-        except KeyError:
-            check = 0
-
-        if check:
-            return successResultJson('test data successfully!')
-        else:
-            return errorResultJson('you are not allowed to do this!')
+        raise Exception('lalalalalalala')
 
     def clearSamples(self, req):
         check = 1
@@ -201,7 +188,7 @@ class Controller(object):
             check = 0
 
         if check:
-            ExperimentInit.getInitialScheme()
+            ExperimentInit().getInitialScheme()
             return successResultJson('initialize experiment successfully, you can launch experiment now!')
         else:
             return errorResultJson('You are not allowed to do this!')
